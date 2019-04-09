53513f081b0bf0d227f157433818849496e43a10
danielhart17 <43150766+danielhart17@users.noreply.github.com>
2019-04-09 00:17:06 -0400

    Update ReadFile.c
    define variables textfile, file and line

--- ReadFile.c
+++ ReadFile.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main(int argc, const char * argv[]) {
+    char const* const textFile = argv[1];
+    FILE* file = fopen(textFile, "r"); // define and open file
+    char line[256]; // define variable 'line' with 256
+    
+    return 0;
+}
